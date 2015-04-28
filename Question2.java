f21b89e6d26032d1060d22ba657fe7cecaa36346
AM021700 <apei0509@gmail.com>
2015-04-28 18:10:59 +0800

    ABC),並送出一個內容為『A AM021700 羅恩培提交Question2

--- Question2.java
+++ Question2.java
@@ -1,7 +1,29 @@
-﻿public class Question2{
-	public static void main(String[] args){
-		txtCopy("Source.txt", "Dest.txt");
+﻿import java.io.*;
+
+public class Tet{
+	public static void main(String[] args) throws IOException{
+		FileReader Sour = new FileReader("Source.txt");
+		BufferedReader bSour = new BufferedReader(Sour);
+		FileWriter  fr_fw = new FileWriter("Dest.txt",true);
+		BufferedWriter br_fw = new BufferedWriter(fr_fw);
+		txtCopy(bSour, br_fw);
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
+		bSour.close();
+		br_fw.close();
+		
+	}
+
+	public static  void txtCopy(BufferedReader bSour,BufferedWriter br_fw)throws IOException{
+		int i;
+		while ((i = bSour.read()) != -1){
+			br_fw.write(i);
+	          
+	}
+
 	}
 }
+
+	
+
+
